b8c8b2464473f9d124618e7152a9e2545df64756
5OL4M3 <70678769+5OL4M3@users.noreply.github.com>
2024-01-25 17:02:14 -0500

    PostCard update

--- Assets/Scripts/PostCard.cs
+++ Assets/Scripts/PostCard.cs
@@ -2,8 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PostCard : MonoBehaviour
+public class PostCard : ScriptableObject
 {
-    public string Title;
+    public string title;
+    public System.DateTime date;
+    //store coordinate
     
+    //store media
+
+    public PostCard(string _title, System.DateTime _date) 
+    {
+        title = _title;
+        date = _date;
+    }
+    
+    //Edit Variables
+    public void updateTitle(string _title)
+    {
+        title = _title;
+    }
+
+    public void displayContent()
+    {
+        Debug.Log("Title: " + title);
+        Debug.Log("Date: " + date);
+    }
 }
