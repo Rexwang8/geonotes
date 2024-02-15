a1531fe833990f0594cca61266b86ee0abae9500
Rexwang8 <rexkingsbackyard@gmail.com>
2024-02-15 14:30:30 -0500

    Testing mainly

--- Assets/Scripts/Tests/ObjectCreationTests.cs
+++ Assets/Scripts/Tests/ObjectCreationTests.cs
@@ -11,73 +11,102 @@ namespace SpatialNotesTest {
 [TestFixture]
 public class ObjectCreationTests
 {
+    // Test the creation of a MapObject and PostCard
    [Test]
    public void test_print() {
          Debug.Log("Hello World");
    }
 
+    // Test the creation of a MapObject and PostCard
     [Test]
     public void InstantiateAndCountNotes()
     {
+        // Clear used asset folder
+        string path = Application.streamingAssetsPath + "/Maps/testmap";
+        if (System.IO.Directory.Exists(path))
+        {
+            System.IO.Directory.Delete(path, true);
+        }
+
         // Create a new map
         MapObject map = new MapObject();
         map.name = "Test Map";
         //pull demomap from streamable assets
-        string mapImagePath = Application.streamingAssetsPath + "/demomap.png";
-        map.notes = new List<PostCard>();
+        string mapImagePath = Application.streamingAssetsPath + "/demomap.jpg";
+        map.CreateMap(_name: "Test Map", _TEMP_IMAGE_PATH: mapImagePath);
 
         // Create a new note
-        PostCard note = new PostCard("Test Note", System.DateTime.Now);
-        map.notes.Add(note);
+        PostCard note = new PostCard(_title: "Test Note", _date: System.DateTime.Now);
+        map.AddPostcard(note);
+        note = new PostCard(_title: "Test Note 2", _date: System.DateTime.Now);
+        map.AddPostcard(note);
 
-        // Display the note
-        note.DisplayContent();
-
-        //show image
-        map.DisplayImage();
-
-        //show map info
-        map.DisplayMapInfo();
 
         Assert.AreEqual(map.name, "Test Map");
-        Assert.AreEqual(map.notes.Count, 1);
+        Assert.AreEqual(map.notes.Count, 2);
         Debug.Log("Test Passed");
     }
 
+    // Test the creation of a MapObject and PostCard
     [Test]
     public void InstantiateSaveLoadCountNotes()
     {
-        // // Create a new map object
-        // MapObject map = new MapObject();
-        // map.name = "Test Map";
-        // string mapImagePath = Application.streamingAssetsPath + "/demomap.jpg";
-        // map.UpdateImage(mapImagePath);
-        // map.UpdateThumbnail(mapImagePath);
-        // map.notes = new List<PostCard>();
+        // Clear used asset folder
+        string path = Application.streamingAssetsPath + "/Maps/testmaptest2";
+        if (System.IO.Directory.Exists(path))
+        {
+            System.IO.Directory.Delete(path, true);
+        }
 
-        // // Create a new note
-        // PostCard note = new PostCard("Test Note MapMaker", System.DateTime.Now);
-        // map.notes.Add(note);
-        // PostCard note2 = new PostCard("Test Note 2 MapMaker", System.DateTime.Now);
-        // map.notes.Add(note2);
-        // map.DisplayMapInfo();
+        // Create a new map object
+        MapObject map = new MapObject();
+        string mapImagePath = Application.streamingAssetsPath + "/demomap.jpg";
+        map.CreateMap(_name: "Test Map Test 2", _TEMP_IMAGE_PATH: mapImagePath);
 
-        // // Save the map
-        // map.SaveMap();
+        // Create a new note
+        PostCard note = new PostCard(_title: "Test Note", _date: System.DateTime.Now);
+        map.AddPostcard(note);
+        note = new PostCard(_title: "Test Note 2", _date: System.DateTime.Now);
+        map.AddPostcard(note);
+        note = new PostCard(_title: "Test Note 3", _date: System.DateTime.Now);
+        map.AddPostcard(note);
 
-        // Debug.Log("Map Created");
+        // Save the map
+        map.SaveNoteJson();
 
-        // MapObject loadedMap = new MapObject();
-        // loadedMap.LoadMap("testmap");
-        // loadedMap.DisplayMapInfo();
-        // for (int i = 0; i < loadedMap.notes.Count; i++)
-        // {
-        //     loadedMap.notes[i].DisplayContent();
-        //     Debug.Log("Datetime: " + loadedMap.notes[i].date + " " + loadedMap.notes[i].dateString + " " + loadedMap.notes[i].timeString);
-        // }
-        // Assert.AreEqual(loadedMap.name, "Test Map");
-        // Assert.AreEqual(loadedMap.notes.Count, 2);
-        // Debug.Log("Test Passed");
+        Debug.Log("Map Created");
+
+        MapObject loadedMap = new MapObject();
+        loadedMap.LoadMap("testmaptest2");
+        loadedMap.DisplayMapInfo();
+        loadedMap.DisplayPostCardInfo();
+        loadedMap.DisplayImage();
+        loadedMap.DisplayThumbnail();
+        loadedMap.DisplayMapInfo();
+        loadedMap.UpdateImage(mapImagePath);
+        loadedMap.UpdateThumbnail(mapImagePath);
+
+        Assert.AreEqual(loadedMap.name, "Test Map Test 2");
+        Assert.AreEqual(loadedMap.notes.Count, 3);
+        Assert.AreEqual(loadedMap.notes[0].title, "Test Note");
+        Assert.AreEqual(loadedMap.notes[1].title, "Test Note 2");
+        Assert.AreEqual(loadedMap.notes[2].title, "Test Note 3");
+        Debug.Log("Test Passed");
+
+    }
+
+    // Test the creation of a MapObject and PostCard
+    [Test]
+    public void InstantiatePostCard()
+    {
+        // Create a new note
+        PostCard note = new PostCard(_title: "Test Note", _date: System.DateTime.Now);
+        note.UpdateTitle("Test Note 2");
+        note.DisplayContent();
+        note.SaveDateTime();
+        note.LoadDateTime();
+        Assert.AreEqual(note.title, "Test Note 2");
+        Debug.Log("Test Passed");
     }
 
 }
---
+++ Assets/Scripts/Tests/UITests.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using SpatialNotes;
+
+namespace SpatialNotesTest
+{
+
+
+    // Test the UI and its functionality
+    [TestFixture]
+    public class UITests : MonoBehaviour
+    {
+        // Test the UI and its functionality
+        [Test]
+        public void test_print()
+        {
+            Debug.Log("Hello World");
+        }
+
+    }
+}
