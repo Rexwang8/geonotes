0cf5e33ad794318f1f8a0afec165f9ec2c78bc04
psankar193 <pran804@gmail.com>
2024-04-18 12:35:17 -0400

    made cursor change when moving the map

--- Assets/Scripts/Cursor_Main.cs
+++ Assets/Scripts/Cursor_Main.cs
@@ -1,29 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using NaughtyAttributes;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using TigerForge;
 
 namespace SpatialNotes
 {
     public class Cursor_Main : MonoBehaviour
     {
-        // Start is called before the first frame update
         public Texture2D cursorNormal;
         public Texture2D cursorQuestionMark;
-        public Texture2D cusorDrag;
+        public Texture2D cursorDrag;
         public Texture2D cursorExit;
-        void _setCursor(Texture2D cursor)
+        public Texture2D customCursor; // New custom cursor for left-click
+
+        void Start()
         {
-            Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+            SetCursor(cursorNormal);
+            EventManager.StartListening("CURSOR_REFRESH", ListenToCursorEvent);
         }
 
-        void _listenToCursorEvent()
+        void Update()
+        {
+            // Check for left mouse button click to change cursor
+            if (Input.GetMouseButtonDown(0)) // 0 corresponds to the left mouse button
+            {
+                SetCursor(customCursor);
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                SetCursor(cursorNormal);
+            }
+        }
+
+        void ListenToCursorEvent()
         {
             string cursorName = EventManager.GetString("CURSOR_NAME");
             Texture2D cursor = cursorNormal;
+
             switch(cursorName)
             {
                 case "NORMAL":
@@ -33,62 +44,26 @@ namespace SpatialNotes
                     cursor = cursorQuestionMark;
                     break;
                 case "DRAG":
-                    cursor = cusorDrag;
+                    cursor = cursorDrag;
                     break;
                 case "EXIT":
                     cursor = cursorExit;
                     break;
-                case "CIRCLE":
-                    cursor = cursorNormal;
-                    DrawThresholdCircle(Color.red, 32);
-                    break;
             }
+
             if (cursor != null && cursorName != "CIRCLE")
-                _setCursor(cursor);
-                return;
+                SetCursor(cursor);
         }
 
-        void Start()
+        void SetCursor(Texture2D cursor)
         {
-            _setCursor(cursorNormal);
-            //DrawThresholdCircle(Color.red, 20);
-            EventManager.StartListening("CURSOR_REFRESH",_listenToCursorEvent);
+            Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
         }
 
-
-
-        void DrawThresholdCircle(Color color, float radius, int thickness = 2)
+        void OnDestroy()
         {
-            //draw a hollow red circle around the cursor
-            Debug.Log("Drawing Circle");
-            Vector2 cursorPos = Input.mousePosition;
-            Texture2D tex = new Texture2D((int)radius * 2, (int)radius * 2);
-            for (int x = 0; x < tex.width; x++)
-            {
-                for (int y = 0; y < tex.height; y++)
-                {
-                    float dist = Vector2.Distance(new Vector2(x, y), new Vector2(tex.width / 2, tex.height / 2));
-                    if (dist > radius - thickness && dist < radius + thickness)
-                    {
-                        tex.SetPixel(x, y, color);
-                    }
-                    else
-                    {
-                        tex.SetPixel(x, y, new Color(0, 0, 0, 0));
-                    }
-                }
-            }
-            //offset the cursor position
-            float offsetX = (tex.width / 2) - (tex.width / 4);
-            float offsetY = (tex.height / 2) - (tex.height / 4);
-            tex.Apply();
-            Cursor.SetCursor(tex, new Vector2(offsetX, offsetY), CursorMode.Auto);
-
-
+            // Reset cursor to default when script is disabled or GameObject is destroyed
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
         }
-
-
-
     }
 }
-
